5a33f2cd8e30cb68149841d18dc99a13633c267f
Shawn Woodford <sw@shawnwoodford.com>
2016-03-15 22:30:17 -0400

    WIP

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ Quirky Wink API tools
 
 [![Build Status](https://travis-ci.org/swoodford/wink.svg?branch=master)](https://travis-ci.org/swoodford/wink)
 
+- **set-credentials.sh** Setup your Wink API credentials
 - **sunrise.sh** Sunrise effect on one lightbulb
 - **wink.sh** Use the Quirky Wink API to control IOT enabled devices
 
--- sunrise.sh
+++ sunrise.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
-# This script uses the Wink API
 
-# This script uses the Wink API to control IOT enabled devices
+# This script uses the Wink API to give a lightbulb a sunrise effect over 10 minutes
 # Requires jq, curl
 
 # Set credentials
@@ -26,35 +25,37 @@ function pause(){
 	echo
 }
 
-function getToken(){
-	getToken=$(curl -sX POST "$APIURL"oauth2/token \
-		-H "Content-Type: application/json" \
-		-d '
-		{
-			"client_id": "'"$client_id"'",
-	    	"client_secret": "'"$client_secret"'",
-		    "username": "'"$username"'",
-		    "password": "'"$password"'",
-		    "grant_type": "password"
-		} ')
-
-	# echo $getToken | jq .
-	access_token=$(echo $getToken | jq '.access_token' | cut -d '"' -f 2)
-	# echo $access_token
-	refresh_token=$(echo $getToken | jq '.refresh_token' | cut -d '"' -f 2)
-	# echo $refresh_token
-}
-
+# Retrieve list of all lightbulbs
 function retrieveLightBulbIDs(){
 	retrieveLightBulbIDs=$(curl -s "$APIURL"/users/me/light_bulbs \
 		-H "Authorization: Bearer $access_token")
 
 	bulbIDs=$(echo $retrieveLightBulbIDs | jq . | grep light_bulb_id | cut -d \" -f4)
+	totalBulbs=$(echo "$bulbIDs" | wc -l)
+	# echo Total Bulbs: "$totalBulbs"
+	bulbNames=$(echo $retrieveLightBulbIDs | jq . | grep -w name | cut -d \" -f4)
+
+	START=1
+	for (( COUNT=$START; COUNT<=$totalBulbs; COUNT++ ))
+	do
+		echo "====================================================="
+		echo \#$COUNT
+		thisBulbID=$(echo "$bulbIDs" | nl | grep -w $COUNT | cut -f 2)
+		echo "Bulb ID: "$thisBulbID
+		thisBulbName=$(echo "$bulbNames" | nl | grep -w $COUNT | cut -f 2)
+		echo "Bulb Name: "$thisBulbName
+
+	# echo "$bulbIDs"
+	# echo "$bulbNames"
+	# pause
+	done
 }
 
 # Turn a light bulb on or off and set brightness level
 function updateLightbulb(){
-	updateLightbulb=$(curl -sX PUT "$APIURL"/light_bulbs/"$1" \
+	echo "$APIURL"/light_bulb/"$1"
+	# pause
+	updateLightbulb=$(curl -sX PUT "$APIURL"/light_bulb/"$1" \
 		-H "Authorization: Bearer $access_token" \
 		-H "Content-Type: application/json" \
 		--data-binary '{
@@ -63,10 +64,12 @@ function updateLightbulb(){
 				"brightness": "'"$3"'"
 			}
 		}')
-	
+	echo $updateLightbulb # | jq .
+	# pause
+
 	result=$(echo $updateLightbulb | jq '.errors != null')
 
-	if [ $result = "true" ]; then
+	if [[ "$result" == "0" ]]; then
 		name=$(echo $updateLightbulb | jq '.data | .name')
 		tput setaf 2; echo $name Successful && tput sgr0
 	else
@@ -75,22 +78,31 @@ function updateLightbulb(){
 }
 
 function sunrise(){
-	echo Fade all Lightbulbs On...
-	for brightnesslevel in 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1
+	echo Sunrise Effect on Lightbulb over 10 minutes...
+
+	for ((i=0; i < 101; i += 1))
 	do
+		echo brightnesslevel: "scale=2; ${i}/100" | bc
+		brightnesslevel=$(echo "scale=2; ${i}/100" | bc)
 		updateLightbulb $bulbid true $brightnesslevel
-		sleep 10
+		sleep 6
+
 	done
 }
 
 # Check required commands
+check_command "bc"
 check_command "jq"
 
 # Get Token
 getToken
 
-# Sunrise
-sunrise
+# retrieveLightBulbIDs
 
-# Do Something
-# updateDevice light_bulbs ###### true
+bulbid="xxxxxxxx"
+brightnesslevel="0"
+
+updateLightbulb $bulbid true $brightnesslevel
+
+# Sunrise
+# sunrise
